ba04cb11228da775b013042b4267a8efb397c42c
nkeeping1996 <nfk.keeping@gmail.com>
2022-12-06 18:32:31 -0500

    Final Solution!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+        File f = new File("src/AOC6Data");
+        FileReader fd = new FileReader(f);
+        BufferedReader br = new BufferedReader(fd);
+        String line;
+
+        while((line=br.readLine())!=null) {
+            String[] readLine = line.split("");
+            for (int i = 0; i < readLine.length - 1; i++) {
+                Set<String> puzzleOne = new HashSet<>();
+                for (int x = i; x < i + 4; x++) {
+                    puzzleOne.add(readLine[x]);
+                }
+                if (puzzleOne.size() == 4) {
+                    System.out.println(i + 4);
+                    break;
+                }
+            }
+            for (int i = 0; i < readLine.length - 1; i++) {
+                Set<String> puzzleTwo = new HashSet<>();
+                for (int x = i; x < i + 14; x++) {
+                    puzzleTwo.add(readLine[x]);
+                }
+                if (puzzleTwo.size() == 14) {
+                    System.out.println(i + 14);
+                    break;
+                }
+
+            }
+        }
+
+        System.out.println("done!");
+        }
     }
-}
+
